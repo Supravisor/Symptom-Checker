d331113dde13feb51e44a3b497252b614e2ceb51
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-09-05 05:22:29 +1200

    asthma

--- script.js
+++ script.js
@@ -7,3 +7,4 @@ const acuteBronchitis = document.getElementById("AcuteBronchitis");
 const aCEInhibitors = document.getElementById("ACEInhibitors");
 const airwayBlockage = document.getElementById("AirwayBlockage");
 const allergies = document.getElementById("Allergies");
+const asthma = document.getElementById("Asthma");
