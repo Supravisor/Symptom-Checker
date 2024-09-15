2880f4c8d21c8c70b78d2616896bdd278a9fd764
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-09-16 10:46:20 +1200

    toggle health issue

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 
 function showTime(arg) {
-  arg.map(el => el.classList.toggle("media-item"));
+  arg.map(el => document.getElementById(el).classList.toggle("media-item"));
 }
-
-const acuteBronchitis = document.getElementById("AcuteBronchitis");
-const aCEInhibitors = document.getElementById("ACEInhibitors");
-const airwayBlockage = document.getElementById("AirwayBlockage");
-const allergies = document.getElementById("Allergies");
-const asthma = document.getElementById("Asthma");
-const bronchitis = document.getElementById("Bronchitis");
-const bronchospasm = document.getElementById("Bronchospasm");
-const chronicObstructivePulmonaryDisease = document.getElementById("ChronicObstructivePulmonaryDisease");
-const cold = document.getElementById("Cold");
-const gastroesophagealRefluxDisease = document.getElementById("GastroesophagealRefluxDisease");
-const glandularFever = document.getElementById("GlandularFever");
-const influenza = document.getElementById("Influenza");
