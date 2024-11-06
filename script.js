d04087a1b0798d292089b97230120d32b76e34d2
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-11-06 15:15:32 +1300

    replace health condition input elements with JS

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 
 const condition = document.getElementById("condition");
+
+let array = [];
+
+function showTime(arg) {
+  array.push(...arg);
+  const filtered = Array.from(new Set(array));
+
+  const sorted = filtered.sort(function(a, b) {
+    return a == b ? 0 : a < b ? -1 : 1;
+  });;
+
+  condition.innerHTML = sorted.map(el => `<input value="${el.replace(/_/g, ' ')}" type="button" onClick="showInfo('${el}')">`).join("");
+
+  return array = sorted;
+}
